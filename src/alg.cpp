7b3b1f1492772847155cf9de4656faa27f23e822
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-03-18 16:10:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
-int sort (int* arr, int size) {
-  for (int  j= 0; j < size-1; j++) {
-    for (int i=0; i<size-1;i++) {
-      if (arr[i] > arr[i+1]) {
+int sort(int* arr, int size) {
+  for (int  j= 0; j < size - 1; j++) {
+    for (int i = 0; i < size - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
         int a = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = a;
@@ -19,16 +19,16 @@ int cbinsearch(int *arr, int size, int value) {
     if (arr[izmeritel] < value) {
       izmeritel = - (izmeritel/2) + size;
     } else if (arr[izmeritel] > value) {
-      izmeritel=izmeritel/2;
+      izmeritel = izmeritel/2;
     } else if (arr[izmeritel] == value) {
-      int counter =1;
-      int one=izmeritel-1;
-      while ( arr[one] == value) {
+      int counter = 1;
+      int one = izmeritel-1;
+      while (arr[one] == value) {
         counter++;
         one--;
       }
-      int two=izmeritel+1;
-      while ( arr[two] == value) {
+      int two = izmeritel+1;
+      while (arr[two] == value) {
         counter++;
         two++;
       }
